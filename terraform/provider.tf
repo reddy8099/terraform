570d4f48ca076b2521f799b830111d3f5f52220e
madhira Ramireddy <madhiraramireddy12@gmail.com>
2023-04-19 07:12:37 +0000

    Updated provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,9 +11,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-subscription_id = "58946ec8-0f9a-463e-a88c-cd43b82daeaa"
-Client_id = "05c70ba8-7db9-4ece-a4b8-98f2d9cd1f96"
-Client_secret = "EFV8Q~AyV1ihNrVsmH7iGHoYhaUhXTJpm9-A9cFQ"
-Tenant _id = "b2b05694-7ebc-412e-ad4e-8c2282741c96"
+subscription_id   = "58946ec8-0f9a-463e-a88c-cd43b82daeaa"
+Client_id         = "05c70ba8-7db9-4ece-a4b8-98f2d9cd1f96"
+Client_secret     = "EFV8Q~AyV1ihNrVsmH7iGHoYhaUhXTJpm9-A9cFQ"
+Tenant_id        = "b2b05694-7ebc-412e-ad4e-8c2282741c96"
 
 }
