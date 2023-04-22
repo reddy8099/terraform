d47f2bf3af6efa8079fc528a42fc75ac3791e37e
madhira Ramireddy <madhiraramireddy12@gmail.com>
2023-04-22 11:31:55 +0000

    Updated Main.tf

--- Kubernetes/Main.tf
+++ Kubernetes/Main.tf
@@ -12,7 +12,7 @@ resource "azurerm_role_assignment" "role_acrpull" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr.name
+  name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "standard"
