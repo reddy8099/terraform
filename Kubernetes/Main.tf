f9ef4302da1effab2dde2aa64b6c792207f44f32
madhira Ramireddy <madhiraramireddy12@gmail.com>
2023-04-22 11:58:11 +0000

    Updated Main.tf

--- Kubernetes/Main.tf
+++ Kubernetes/Main.tf
@@ -7,7 +7,7 @@ resource "azurerm_role_assignment" "role_acrpull" {
     scope                             = azurerm_container_registry.acr.id
     role_definition_name              = "Acrpull"
     principal_id                      = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-    skip_service_principal_aaa_check  = true
+    skip_service_principal_aad_check  = true
 
 }
 
