9c535260575c6c2d10130a9c4c1bc6970364f96d
madhira Ramireddy <madhiraramireddy12@gmail.com>
2023-04-22 09:59:00 +0000

    Updated variable.tf

--- Kubernetes/variable.tf
+++ Kubernetes/variable.tf
@@ -1,1 +1,31 @@
-asdkjgvbhausdfkjbgaskjd.gvg,
+variable "resource_group_name"{
+    type = string
+    description = "RG name in Azure"
+
+}
+
+variable "location" {
+    type = string
+    description = "Resource loacion in Azure"
+
+}
+variable "cluster_name" {
+    type = string
+    description = "AKS name in Azure"
+
+}
+variable "kubernetes_version" {
+    type = string
+    description = "kubernetes version"
+
+}
+variable "system_node_count" {
+    type = string
+    description = "number of AKS worker nodes"
+
+}
+variable "acr_name" {
+    type = string
+    description = "ACR name"
+
+}
