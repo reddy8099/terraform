f7aecf4a7e2fd187fe6a85d3b9a7ad89a763bf73
madhira Ramireddy <madhiraramireddy12@gmail.com>
2023-05-13 02:52:20 +0000

    Added main.tf

---
+++ single virtual machine/main.tf
@@ -0,0 +1,66 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "radha"
+  location = "East us"
+}
+resource "azurerm_public_ip" "pip" {
+  name                = "rampip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "hubbvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "hubdsub"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "shivnic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "sivaip"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_network_security_group" "nsg" {
+  name = "ram-nsg"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+}
+
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "vm-possible"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  admin_password      = "Password@1234"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
