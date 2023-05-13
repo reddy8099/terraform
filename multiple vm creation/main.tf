4bc270ec1f6003611695f10a8f9582882d4a97bb
madhira Ramireddy <madhiraramireddy12@gmail.com>
2023-05-13 14:51:50 +0000

    Updated main.tf

--- multiple vm creation/main.tf
+++ multiple vm creation/main.tf
@@ -1,9 +1,24 @@
-resource "azurerm_storage_account" "sa" {
- count                 = var.count 
- name                  = "acctvm${count.index}"
- location              = azurerm_resource_group.rg.location
- availability_set_id   = azurerm_availability_set.avset.id
- resource_group_name   = azurerm_resource_group.rg.name
- network_interface_ids = [element(azurerm_network_interface.rg.*.id, count.index)]
- vm_size               = "Standard_DS1_v2"
- }
+
+ resource "azurerm_windows_virtual_machine" "vm" {
+  for_each            = var.vm
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.demo-rg.name
+  location            = var.location
+  size                = each.value.size
+  admin_username      = var.vm_admin_login
+  admin_password      = var.vm_admin_password
+
+  network_interface_ids = [azurerm_network_interface.nic[each.key].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+   source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
